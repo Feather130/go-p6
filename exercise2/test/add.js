6f198deb92c5d9f14e6e7cb72e8083327a67c355
Feather130 <1301788346@qq.com>
2018-07-22 18:21:11 +0800

    Add files via upload

---
+++ exercise2/test/add.js
@@ -0,0 +1,21 @@
+function add(num1, num2) {
+  // 实现该函数
+  /* let arr1 = (num1.split('')).reverse()
+  let arr2 = (num2.split('')).reverse()
+  let max = Math.max(arr1.length, arr2.length)
+  let arr3 = []
+  let flog = 0
+  for (let i = 0; i < max; i++) {
+    let num = (+arr1[i] || 0) + (+arr2[i] || 0) + flog
+    flog = 0
+    if (num > 9 && i !== max - 1) {
+      flog = 1
+      num -= 10
+    }
+    arr3.push(num)
+  }
+  return arr3.reverse().join('') */
+
+
+}
+module.exports = add
